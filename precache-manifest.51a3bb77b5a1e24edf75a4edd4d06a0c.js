2a9fc6e1bc52963e5e410217c21160b0a0aac062
igazzaz <44285332+igazzaz@users.noreply.github.com>
2018-11-11 03:14:19 +0300

    Add files via upload

---
+++ precache-manifest.51a3bb77b5a1e24edf75a4edd4d06a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "73359565e029f67bfe3e",
+    "url": "/static/js/main.73359565.chunk.js"
+  },
+  {
+    "revision": "8c8e9863edf0dd9975fc",
+    "url": "/static/js/1.8c8e9863.chunk.js"
+  },
+  {
+    "revision": "73359565e029f67bfe3e",
+    "url": "/static/css/main.05b3ca82.chunk.css"
+  },
+  {
+    "revision": "8c8e9863edf0dd9975fc",
+    "url": "/static/css/1.e8540436.chunk.css"
+  },
+  {
+    "revision": "35a5de3b71a425166e0aa160dba07f0e",
+    "url": "/index.html"
+  }
+];
